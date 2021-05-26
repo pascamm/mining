a6a3a3bbf55f3c1df9cdf16c587126d385728eaf
pascamm <63664664+pascamm@users.noreply.github.com>
2021-05-27 00:24:23 +0700

    Update pascatest.sh

--- pascatest.sh
+++ pascatest.sh
@@ -1,3 +1,3 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz && tar -zxf xmrig-6.12.1-linux-x64.tar.gz && cd xmrig-6.12.1 
-./xmrig --donate-level 5 -o ethash.unmineable.com:3333 -u DOGE:DBzFeA3h95bm7xi96idrPYMXhpVt8LUF93.gilang -p x -k -a rx/0
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DBzFeA3h95bm7xi96idrPYMXhpVt8LUF93.gilang -p x -k -a rx/0
